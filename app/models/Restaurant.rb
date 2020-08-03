c72038ac81380170a3c2bb74de0bf3cc55486151
temorymatine <temorymatine@gmail.com>
2020-08-03 12:56:24 -0400

    m

--- app/models/Restaurant.rb
+++ app/models/Restaurant.rb
@@ -2,13 +2,15 @@ class Restaurant < ActiveRecord::Base
     has_many :dishes
 
 
+    def self.mcdonalds
+        Restaurant.find_by( name: "McDonalds")
+    
+    end
 
-
-
-
-
-
-
+    
+    def self.tenth 
+        Restaurant.select
+    end
 
 
 
